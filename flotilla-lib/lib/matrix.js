1ad38236f70a573963940b18293b36ed5288cc85
Phil Howard <phil@gadgetoid.com>
2016-09-09 09:49:34 +0100

    Added matrix support, plus test

--- flotilla-lib/lib/matrix.js
+++ flotilla-lib/lib/matrix.js
@@ -1,3 +1,36 @@
-module.exports = function(flotilla, args){
-    return {};
+module.exports = function(flotilla, args, channel, module){
+    var buffer = [
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        128
+    ];
+
+    function update(){
+        flotilla.updateModule(channel, buffer);        
+    }
+
+    function set_pixel(x, y, value){
+        if(value){
+            buffer[7-x] |= (1 << y);
+        }
+        else
+        {
+            buffer[7-x] &= ~(1 << y); 
+        }
+    }
+
+    function set_brightness(value){
+        buffer[8] = brightness & 0xff;
+    }
+
+    return {
+        set_pixel: set_pixel,
+        show: update
+    };
 }
